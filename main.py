e8f3b34ba592bbe45074d0df7bc65f198afe58a4
Allen Junior <168249812+allenjr5@users.noreply.github.com>
2024-10-14 18:40:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -30,21 +30,33 @@ def mostra_tabuleiro():
         for c in range(n_colunas):
             print(tabuleiro[l][c], end=' ')
         print()
-    time.sleep(0.5)
+    time.sleep(0.3)
 
 #Função que adiciona uma peça no tabuleiro e move automaticamente para baixo
 def adiciona_peca(peca, py, px):
     for l in range(len(peca)):
-        for c in range(len(peca[l])):
+        for c in range(len(peca[0])):
             if peca[l][c]=='1':
                 tabuleiro[l+py][c+px]='🟦'
 
 #Função que limpa a posição anterior da peça no tabuleiro
 def limpa_peca(peca, py, px):
     for l in range(len(peca)):
-        for c in range(len(peca[l])):
+        for c in range(len(peca[0])):
             if peca[l][c]=='1':
                 tabuleiro[l+py][c+px]='⬛'
+
+#Função que define a posição y máxima para cada tipo de peça
+def py_max(peca):
+    return 15 if len(peca)==4 else 16 if len(peca)==3 else 17
+
+#Função que define a posição x máxima para cada tipo de peça
+def px_max(peca):
+    return 7 if len(peca[0])==3 else 8 if len(peca[0])==2 else 9
+
+#Função que chaca a colisão da peça no final do tabuleiro
+def checa_colisao(peca, py, py_max, px):
+    return True if py<=py_max else False
 
 #Função que limpa o terminal
 def limpa_terminal():
@@ -54,14 +66,15 @@ def limpa_terminal():
 def main():
     while True:
         peca=random.choice(pecas)
-        posicao_y=0
-        posicao_x=random.randint(0, 9 - len(peca[0]))
-        while True:
-            adiciona_peca(peca, posicao_y, posicao_x)
+        py=0
+        px=random.randint(0, px_max(peca))
+        adiciona_peca(peca, py, px)
+        while checa_colisao(peca, py, py_max(peca), px):
             mostra_tabuleiro()
-            limpa_peca(peca, posicao_y, posicao_x)
-            posicao_y+=1
+            limpa_peca(peca, py, px)
+            py+=1
             limpa_terminal()
+            adiciona_peca(peca, py, px)
 
 #Chamada da função principal do programa
 main()
