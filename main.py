9fc51d7994e39a25985cfb1483e021b6445d77a6
Allen Junior <168249812+allenjr5@users.noreply.github.com>
2024-10-15 17:52:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-#Importação de bibliotecas que serão utilizadas no código
+#Importação de Bibliotecas
 import os, time, random
 
-#Declaração de variáveis que serão utilizadas com frequência no código
-n_linhas=20
-n_colunas=10
+#Declaração de Variáveis Globais
+n_linhas = 20
+n_colunas = 10
 
-#Inicialização da matriz do tabuleiro
-tabuleiro=[]
-for l in range(n_linhas):
-    linha=[]
-    for c in range(n_colunas):
-        linha.append('⬛')
-    tabuleiro.append(linha)
+#Inicialização do Tabuleiro
+tabuleiro = [['⬛' for _ in range(n_colunas)] for _ in range(n_linhas)]
 
-#Inicialização das matrizes das peças
-pecas=[
+#Inicialização das Peças
+pecas = [
     ['1', '1', '1', '1'],
     [['1', '1'], ['1', '1']],
     [['1', '1', '1'], ['0', '1', '0']],
@@ -24,57 +19,58 @@ pecas=[
     [['0', '0', '1'], ['1', '1', '1']]
 ]
 
-#Função que mostra o tabuleiro no terminal
+#Função que Mostra o Tabuleiro no Terminal
 def mostra_tabuleiro():
     for l in range(n_linhas):
         for c in range(n_colunas):
             print(tabuleiro[l][c], end=' ')
         print()
-    time.sleep(0.3)
+    time.sleep(0.1)
 
-#Função que adiciona uma peça no tabuleiro e move automaticamente para baixo
+#Função que Adiciona uma Peça
 def adiciona_peca(peca, py, px):
     for l in range(len(peca)):
         for c in range(len(peca[0])):
-            if peca[l][c]=='1':
-                tabuleiro[l+py][c+px]='🟦'
+            if peca[l][c] == '1':
+                tabuleiro[py + l][px + c] = '🟦'
 
-#Função que limpa a posição anterior da peça no tabuleiro
+#Função que Limpa a Posição Anterior da Peça
 def limpa_peca(peca, py, px):
     for l in range(len(peca)):
         for c in range(len(peca[0])):
-            if peca[l][c]=='1':
-                tabuleiro[l+py][c+px]='⬛'
+            if peca[l][c] == '1':
+                tabuleiro[py + l][px + c] = '⬛'
 
-#Função que define a posição y máxima para cada tipo de peça
-def py_max(peca):
-    return 15 if len(peca)==4 else 16 if len(peca)==3 else 17
+#Função que Verifica a Colisão da Peça
+def verifica_colisao(peca, py, px):
+    for l in range(len(peca)):
+        for c in range(len(peca[0])):
+            if peca[l][c] == '1':
+                if py + l >= n_linhas or tabuleiro[py + l][px + c] == '🟦':
+                    return True
+    return False
 
-#Função que define a posição x máxima para cada tipo de peça
-def px_max(peca):
-    return 7 if len(peca[0])==3 else 8 if len(peca[0])==2 else 9
-
-#Função que chaca a colisão da peça no final do tabuleiro
-def checa_colisao(peca, py, py_max, px):
-    return True if py<=py_max else False
-
-#Função que limpa o terminal
+#Função que Limpa o Terminal
 def limpa_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-#Função principal do programa
+#Função Principal
 def main():
     while True:
-        peca=random.choice(pecas)
-        py=0
-        px=random.randint(0, px_max(peca))
-        adiciona_peca(peca, py, px)
-        while checa_colisao(peca, py, py_max(peca), px):
+        peca = random.choice(pecas)
+        posicao_y = 0
+        posicao_x = random.randint(0, n_colunas - len(peca[0]))
+        adiciona_peca(peca, posicao_y, posicao_x)
+        while True:
             mostra_tabuleiro()
-            limpa_peca(peca, py, px)
-            py+=1
+            limpa_peca(peca, posicao_y, posicao_x)
+            if not verifica_colisao(peca, posicao_y + 1, posicao_x):
+                posicao_y += 1
+            else:
+                adiciona_peca(peca, posicao_y, posicao_x)
+                break
             limpa_terminal()
-            adiciona_peca(peca, py, px)
+            adiciona_peca(peca, posicao_y, posicao_x)
 
-#Chamada da função principal do programa
+#Chamada da Função Principal
 main()
